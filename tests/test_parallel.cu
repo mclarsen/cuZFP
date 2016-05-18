d217c0cd7a47554002fa576c9d8d760f397a2a8d
Mark Kim <mbk@cs.utah.edu>
2016-05-18 09:57:06 -0600

    Timing output.
    Remove dead code.

--- tests/test_parallel.cu
+++ tests/test_parallel.cu
@@ -22,9 +22,9 @@ using namespace std;
 
 #define index(x, y, z) ((x) + 4 * ((y) + 4 * (z)))
 
-const size_t nx = 64;
-const size_t ny = 64;
-const size_t nz = 64;
+const size_t nx = 512;
+const size_t ny = 512;
+const size_t nz = 512;
 
 
 //BSIZE is the length of the array in class Bit
@@ -162,73 +162,6 @@ void setupConst(const unsigned char *perm,
 
 }
 
-template<class Int, class UInt, class Scalar, uint bsize>
-void cpuEFPDI2UTransform
-(
-const dim3 &emax_size,
-const dim3 &blockDim,
-const dim3 &gridDim,
-const Scalar *data,
-UInt *p,
-cuZFP::Bit<bsize> *stream
-
-)
-{
-	uint3 blockIdx;
-
-	for (blockIdx.z = 0; blockIdx.z < gridDim.z; blockIdx.z++){
-		for (blockIdx.y = 0; blockIdx.y < gridDim.y; blockIdx.y++){
-			for (blockIdx.x = 0; blockIdx.x < gridDim.x; blockIdx.x++){
-				uint3 threadIdx;
-				//extern __shared__ long long sh_q[];
-				long long *sh_q = new long long[64*64];
-				for (threadIdx.z = 0; threadIdx.z < blockDim.z; threadIdx.z++){
-					for (threadIdx.y = 0; threadIdx.y < blockDim.y; threadIdx.y++){
-						for (threadIdx.x = 0; threadIdx.x < blockDim.x; threadIdx.x++){
-							int mx = threadIdx.x + blockDim.x*blockIdx.x;
-							int my = threadIdx.y + blockDim.y*blockIdx.y;
-							int mz = threadIdx.z + blockDim.z*blockIdx.z;
-							int eidx = mz*gridDim.x*blockDim.x*gridDim.y*blockDim.y + my*gridDim.x*blockDim.x + mx;
-
-							mx *= 4; my *= 4; mz *= 4;
-							//int idx = z*gridDim.x*gridDim.y*blockDim.x*blockDim.y*16 + y*gridDim.x*blockDim.x*4+ x;
-							int emax = max_exp_block(data, mx, my, mz, 1, gridDim.x*blockDim.x * 4, gridDim.x*blockDim.x * 4 * gridDim.y*blockDim.y * 4);
-
-							stream[eidx].emax = emax;
-							//	uint sz = gridDim.x*blockDim.x * 4 * gridDim.y*blockDim.y * 4;
-							//	uint sy = gridDim.x*blockDim.x * 4;
-							//	uint sx = 1;
-							fixed_point_block(sh_q + (threadIdx.x + threadIdx.y * 4 + threadIdx.z * 16) * 64, data, emax, mx, my, mz, 1, gridDim.x*blockDim.x * 4, gridDim.x*blockDim.x * 4 * gridDim.y*blockDim.y * 4);
-							fwd_xform(sh_q + (threadIdx.x + threadIdx.y * 4 + threadIdx.z * 16) * 64);
-
-
-							//fwd_order
-							for (int i = 0; i < 64; i++){
-								uint idx = eidx * 64 + i;
-								p[idx] = cuZFP::int2uint<Int, UInt>(sh_q[(threadIdx.x + threadIdx.y * 4 + threadIdx.z * 16) * 64 + perm[i]]);
-							}
-						}
-					}
-				}
-				delete[]sh_q;
-
-
-			}
-		}
-	}
-}
-
-
-/* reorder unsigned coefficients and convert to signed integer */
-template<class Int, class UInt>
-__host__
-static void
-inv_order(const UInt* ublock, Int* iblock, const unsigned char* perm, uint n)
-{
-	do
-		iblock[*perm++] = cuZFP::uint2int<UInt>(*ublock++);
-	while (--n);
-}
 
 
 //Used to generate rand array in CUDA with Thrust
@@ -244,134 +177,6 @@ struct RandGen
 		return uniDist(randEng);
 	}
 };
-template<class Int, class UInt>
-void reorder
-(
-const Int *q,
-UInt *buffer
-)
-{
-	for (uint i = 0; i < 64; i++)
-		buffer[i] = cuZFP::int2uint<Int, UInt>(q[perm[i]]);
-}
-
-
-template<class UInt, uint bsize>
-void cpuEncodeUInt
-(
-const unsigned long long count,
-uint size,
-const UInt* data,
-const unsigned char *g_cnt,
-cuZFP::Bit<bsize> *stream
-)
-{
-
-	//extern __shared__ unsigned char smem[];
-	//__shared__ unsigned char *sh_g, *sh_sbits;
-	//__shared__ Bitter *sh_bitters;
-
-	//sh_g = &smem[0];
-	//sh_sbits = &smem[64];
-	//sh_bitters = (Bitter*)&smem[64 + 64];
-
-	//uint tid = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z *blockDim.x*blockDim.y;
-
-	//uint bidx = (blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.y * gridDim.x)*blockDim.x*blockDim.y*blockDim.z;
-
-	uint3 blockIdx, gridDim, blockDim;
-	gridDim.x = nx / 4;
-	gridDim.y = ny / 4;
-	gridDim.z = nz / 4;
-	
-	blockDim.x = blockDim.y = blockDim.z = 4;
-
-	for (blockIdx.z = 0; blockIdx.z < gridDim.z; blockIdx.z++){
-		for (blockIdx.y = 0; blockIdx.y < gridDim.y; blockIdx.y++){
-			for (blockIdx.x = 0; blockIdx.x <gridDim.x; blockIdx.x++){
-				uint bidx = (blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.y * gridDim.x)*blockDim.x*blockDim.y*blockDim.z;
-
-				unsigned long long x[64];
-				Bitter bitter[64];
-				unsigned char sbit[64];
-				for (int i = 0; i < 64; i++){
-					bitter[i] = make_bitter(0, 0);
-					sbit[i] = 0;
-				}
-				uint s_emax_bits[1];
-				s_emax_bits[0] = 1;
-				//maxprec, minexp, EBITS
-				//	uint k = threadIdx.x + blockDim.x * blockIdx.x;
-				int emax = stream[bidx / 64].emax;
-				int maxprec = precision(emax, MAXPREC, MINEXP);
-				int ebits = EBITS + 1;
-				const uint kmin = intprec > maxprec ? intprec - maxprec : 0;
-
-				uint e = maxprec ? emax + ebias : 0;
-				//printf("%d %d %d %d\n", emax, maxprec, ebits, e);
-				if (e){
-					//write_bitters(bitter[0], make_bitter(2 * e + 1, 0), ebits, sbit[0]);
-					stream[bidx / 64].begin[0] = 2 * e + 1;
-					s_emax_bits[0] = ebits;
-				}
-//				const uint kmin = intprec > MAXPREC ? intprec - MAXPREC : 0;
-
-				unsigned long long y[64];
-				for (int tid = 0; tid < 64; tid++){
-					/* extract bit plane k to x[k] */
-					y[tid] = 0;
-					for (uint i = 0; i < size; i++)
-						y[tid] += ((data[bidx + i] >> tid) & (unsigned long long)1) << i;
-					x[tid] = y[tid];
-				}
-
-				char sh_g[64], sh_sbits[64];
-				Bitter sh_bitters[64];
-
-				/* count number of positive group tests g[k] among 3*d in d dimensions */
-				for (int tid = 0; tid < 64; tid++){
-					sh_g[tid] = 0;
-					for (unsigned long long c = count; y[tid]; y[tid] >>= c & 0xfu, c >>= 4)
-						sh_g[tid]++;
-				}
-
-
-				unsigned char cur = sh_g[intprec - 1];
-
-				for (int i = intprec - 1; i-- > kmin;) {
-					if (cur < sh_g[i])
-						cur = sh_g[i];
-					else if (cur > sh_g[i])
-						sh_g[i] = cur;
-				}
-
-				for (int tid = 0; tid < 64; tid++){
-					unsigned char g = sh_g[tid];
-					unsigned char h = sh_g[min(tid + 1, intprec - 1)];
-
-
-					encodeBitplane(count, x[tid], g, h, g_cnt, bitter[tid], sbit[tid]);
-					sh_bitters[63 - tid] = bitter[tid];
-					sh_sbits[63 - tid] = sbit[tid];
-				}
-
-
-				uint tot_sbits = s_emax_bits[0];
-				uint offset = 0;
-				for (int i = 0; i < intprec; i++){
-					if (sh_sbits[i] <= 64){
-						write_outx(sh_bitters, stream[bidx / 64].begin, tot_sbits, offset, i, sh_sbits[i]);
-					}
-					else{
-						write_outx(sh_bitters, stream[bidx / 64].begin, tot_sbits, offset, i, 64);
-						write_outy(sh_bitters, stream[bidx / 64].begin, tot_sbits, offset, i, sh_sbits[i] - 64);
-					}
-				}
-			}
-		}
-	}
-}
-
 
 template<class Int, class UInt, class Scalar, uint bsize>
 void cpuEncode
@@ -385,18 +190,6 @@ const unsigned char *g_cnt,
 cuZFP::Bit<bsize> *stream
 )
 {
-
-	//extern __shared__ unsigned char smem[];
-	//__shared__ unsigned char *sh_g, *sh_sbits;
-	//__shared__ Bitter *sh_bitters;
-
-	//sh_g = &smem[0];
-	//sh_sbits = &smem[64];
-	//sh_bitters = (Bitter*)&smem[64 + 64];
-
-	//uint tid = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z *blockDim.x*blockDim.y;
-
-	//uint bidx = (blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.y * gridDim.x)*blockDim.x*blockDim.y*blockDim.z;
 
 	dim3 blockIdx;
 	
@@ -550,100 +343,6 @@ cuZFP::Bit<bsize> *stream
 		}
 	}
 }
-template<class Int, class UInt, uint bsize, uint num_sidx>
-void cpuDecodeInvOrder
-(
-size_t *sidx,
-cuZFP::Bit<bsize> *stream,
-
-Int *data,
-
-const unsigned long long orig_count
-
-)
-{
-	//uint tid = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z *blockDim.x*blockDim.y;
-	//uint idx = (blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.y * gridDim.x);
-	//uint bdim = blockDim.x*blockDim.y*blockDim.z;
-	//uint bidx = idx*bdim;
-
-	uint3 blockIdx, gridDim, blockDim;
-	gridDim.x = nx / 4;
-	gridDim.y = ny / 4;
-	gridDim.z = nz / 4;
-
-	blockDim.x = blockDim.y = blockDim.z = 4;
-
-	for (blockIdx.z = 0; blockIdx.z < gridDim.z; blockIdx.z++){
-		for (blockIdx.y = 0; blockIdx.y < gridDim.y; blockIdx.y++){
-			for (blockIdx.x = 0; blockIdx.x < gridDim.x; blockIdx.x++){
-				uint idx = (blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.y * gridDim.x);
-				uint bdim = blockDim.x*blockDim.y*blockDim.z;
-				uint bidx = idx*bdim;
-
-				size_t s_sidx[64];// = (size_t*)&smem[0];
-				//if (tid < num_sidx)
-				for (int tid = 0; tid < num_sidx; tid++){
-
-					s_sidx[tid] = sidx[tid];
-				}
-
-				uint s_idx_n[64];// = (uint*)&smem[s_sidx[0]];
-				uint s_idx_g[64];// = (uint*)&smem[s_sidx[1]];
-				unsigned long long s_bit_cnt[64];// = (unsigned long long*)&smem[s_sidx[2]];
-				uint s_bit_rmn_bits[64];// = (uint*)&smem[s_sidx[3]];
-				char s_bit_offset[64];// = (char*)&smem[s_sidx[4]];
-				uint s_bit_bits[64];// = (uint*)&smem[s_sidx[5]];
-				Word s_bit_buffer[64];// = (Word*)&smem[s_sidx[6]];
-				UInt s_data[64];// = (UInt*)&smem[s_sidx[7]];
-				uint s_kmin[1];
-
-
-				stream[idx].read_bit();
-				uint ebits = EBITS + 1;
-				int emax = stream[idx].read_bits(ebits - 1) - ebias;
-				int maxprec = precision(emax, MAXPREC, MINEXP);
-				s_kmin[0] = intprec > maxprec ? intprec - maxprec : 0;
-
-				
-				for (int tid = 0; tid < 64; tid++){
-					s_idx_g[tid] = 0;
-					s_data[tid] = 0;
-				}
-
-				cuZFP::insert_bit<bsize>(
-					stream[idx],
-					s_idx_g,
-					s_idx_n,
-					s_bit_bits,
-					s_bit_offset,
-					s_bit_buffer,
-					s_bit_cnt,
-					s_bit_rmn_bits,
-					MAXBITS - ebits, intprec, s_kmin[0], orig_count);
-
-				for (int tid = 0; tid < 64; tid++){
-
-					for (uint k = s_kmin[0]; k < intprec; k++){
-						cuZFP::decodeBitstream<UInt, bsize>(
-							stream[idx],
-							s_idx_g[k],
-							s_idx_n[k],
-							s_bit_cnt[k],
-							s_bit_rmn_bits[k],
-							s_bit_bits[k],
-							s_bit_offset[k],
-							s_bit_buffer[k],
-							s_data[tid],
-							tid, k);
-					}
-
-					data[perm[tid] + bidx] = cuZFP::uint2int<Int, UInt>(s_data[tid]);
-				}
-			}
-		}
-	}
-}
 
 template<class Int, class UInt, class Scalar, uint bsize, uint num_sidx>
 void cpuDecode
@@ -658,10 +357,6 @@ const unsigned long long orig_count
 
 )
 {
-	//uint tid = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z *blockDim.x*blockDim.y;
-	//uint idx = (blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.y * gridDim.x);
-	//uint bdim = blockDim.x*blockDim.y*blockDim.z;
-	//uint bidx = idx*bdim;
 
 	dim3 blockIdx;
 
@@ -727,111 +422,13 @@ const unsigned long long orig_count
 					s_q[perm[tid]] = cuZFP::uint2int<Int, UInt>(s_data[tid]);
 
 				}
-				//for (int tid = 0; tid < 64; tid++){
-				//	s_idx_g[tid] = 0;
-				//	s_data[tid] = 0;
-				//}
 
-				//cuZFP::insert_bit<bsize>(
-				//	stream[idx],
-				//	s_idx_g,
-				//	s_idx_n,
-				//	s_bit_bits,
-				//	s_bit_offset,
-				//	s_bit_buffer,
-				//	s_bit_cnt,
-				//	s_bit_rmn_bits,
-				//	MAXBITS - ebits, intprec, s_kmin[0], orig_count);
-
-				//for (int tid = 0; tid < 64; tid++){
-
-				//	for (uint k = s_kmin[0]; k < intprec; k++){
-				//		cuZFP::decodeBitstream<UInt, bsize>(
-				//			stream[idx],
-				//			s_idx_g[k],
-				//			s_idx_n[k],
-				//			s_bit_cnt[k],
-				//			s_bit_rmn_bits[k],
-				//			s_bit_bits[k],
-				//			s_bit_offset[k],
-				//			s_bit_buffer[k],
-				//			s_data[tid],
-				//			tid, k);
-				//	}
-
-				//	s_q[perm[tid]] = cuZFP::uint2int<Int, UInt>(s_data[tid]);
-
-
-
-
-				//}
 
 				uint mx = blockIdx.x, my = blockIdx.y, mz = blockIdx.z;
 				mx *= 4; my *= 4; mz *= 4;
 
 				cuZFP::inv_xform(s_q);
 				cuZFP::inv_cast<Int, Scalar>(s_q, out, s_emax[0], mx, my, mz, 1, gridDim.x*blockDim.x, gridDim.x*blockDim.x * gridDim.y*blockDim.y);
-
-			}
-		}
-	}
-}
-template<class Int, class UInt, class Scalar, uint bsize>
-void gpuValidate
-(
-host_vector<Scalar> &h_p,
-device_vector<Int> &q,
-device_vector<Scalar> &data
-)
-{
-	host_vector<Int> h_q;
-
-	h_q = q;
-
-	int i = 0;
-	for (int z = 0; z < nz; z += 4){
-		for (int y = 0; y < ny; y += 4){
-			for (int x = 0; x < nx; x += 4){
-				int idx = z*nx*ny + y*nx + x;
-				host_vector<Int> q2(64);
-				host_vector<UInt> buf(64);
-				cuZFP::Bit<bsize> loc_stream;
-				int emax2 = cuZFP::max_exp_block<Scalar>(raw_pointer_cast(h_p.data()), x, y, z, 1, nx, nx*ny);
-				fixed_point_block(raw_pointer_cast(q2.data()), raw_pointer_cast(h_p.data()), emax2, x, y, z, 1, nx, nx*ny);
-				fwd_xform(raw_pointer_cast(q2.data()));
-				reorder<Int, UInt>(raw_pointer_cast(q2.data()), raw_pointer_cast(buf.data()));
-				cuZFP::encode_ints<UInt>(loc_stream, raw_pointer_cast(buf.data()), minbits, MAXBITS, precision(emax2, maxprec, minexp), group_count, size);
-
-				loc_stream.rewind();
-				UInt dec[64];
-
-				cuZFP::decode_ints<UInt, bsize>(loc_stream, dec, minbits, MAXBITS, precision(emax2, maxprec, minexp), group_count, size);
-
-
-				Int iblock[64];
-				inv_order(dec, iblock, perm, 64);
-				inv_xform(iblock);
-
-				for (int j = 0; j < 64; j++){
-					assert(h_q[i * 64 + j] == iblock[j]);
-				}
-
-				Scalar fblock[64];
-				inv_cast(iblock, fblock, emax2, 0, 0, 0, 1, 4, 16);
-
-				int fidx = 0;
-				for (int k = z; k < z + 4; k++){
-					for (int j = y; j < y + 4; j++){
-						for (int i = x; i < x + 4; i++, fidx++){
-							if (h_p[k*nz*ny + j*ny + i] != fblock[fidx]){
-								cout << "inv_cast failed: " << k << " " << j << " " << i << " " << fidx << " " << h_p[k*nz*ny + j*ny + i] << " " << fblock[fidx] << endl;
-								exit(-1);
-							}
-
-						}
-					}
-				}
-				i++;
 
 			}
 		}
@@ -887,7 +484,7 @@ host_vector<Scalar> &h_data
 
 	cout << "encode GPU in time: " << millisecs << endl;
 
-	host_vector<cuZFP::Bit<bsize>> cpu_stream;
+  thrust::host_vector<cuZFP::Bit<bsize> > cpu_stream;
 	cpu_stream = stream;
 	UInt sum = 0;
 	for (int i = 0; i < cpu_stream.size(); i++){
@@ -1079,11 +676,11 @@ int main()
 	//    cudaDeviceSetCacheConfig(cudaFuncCachePreferL1);
 	setupConst<double>(perm, MAXBITS, MAXPREC, MINEXP, EBITS);
 	cout << "Begin gpuTestBitStream" << endl;
-	//gpuTestBitStream<long long, unsigned long long, double, BSIZE>(h_vec_in);
+  gpuTestBitStream<long long, unsigned long long, double, BSIZE>(h_vec_in);
 	cout << "Finish gpuTestBitStream" << endl;
 
 	cout << "Begin cpuTestBitStream" << endl;
-	cpuTestBitStream<long long, unsigned long long, double, BSIZE>(h_vec_in);
+  //cpuTestBitStream<long long, unsigned long long, double, BSIZE>(h_vec_in);
 	cout << "Finish cpuTestBitStream" << endl;
 
 
@@ -1107,6 +704,7 @@ int main()
 	zfp_stream_set_bit_stream(stream, s);
 	zfp_stream_rewind(stream);
 
+  double start_time = omp_get_wtime();
 	int m = 0;
 	for (int z = 0; z < nz; z += 4){
 		for (int y = 0; y < ny; y += 4){
@@ -1126,12 +724,14 @@ int main()
 			}
 		}
 	}
-
+  double time = omp_get_wtime() - start_time;
+  cout << "encode time: " << time << endl;
 	//cout << "sum UInt " << thrust::reduce(stream->begin, stream->end) << endl;
 	stream_flush(s);
 
 	host_vector<double> h_out(nx*ny*nz);
 	stream_rewind(s);
+  start_time = omp_get_wtime();
 	for (int z = 0; z < nz; z += 4){
 		for (int y = 0; y < ny; y += 4){
 			for (int x = 0; x < nx; x += 4){
@@ -1148,6 +748,8 @@ int main()
 			} 
 		}
 	}
+  time = omp_get_wtime() - start_time;
+  cout << "encode time: " << time << endl;
 
 	double tot_diff = 0;
 	for (int i = 0; i < nx*ny*nz; i++){
