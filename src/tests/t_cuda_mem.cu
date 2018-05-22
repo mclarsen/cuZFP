6469e5267ebd442e56b6f57cec40d8bc8d34f02b
mclarsen <larsen30@llnl.gov>
2018-05-22 07:46:05 -0700

    adding example keeping comressed stream on the gpu.

--- src/tests/t_cuda_mem.cu
+++ src/tests/t_cuda_mem.cu
@@ -8,19 +8,16 @@
 
 using namespace cuZFP;
 
-TEST(sanity_check_float32, test_sanity_check_float32)
+TEST(t_mem_detection, t_device_mem_check)
 {
   //
   // this test is a simple sanity check to see if
   // we can actually encode and decode with block size.
   // that is not a multiple of four.
   //
-  int x = 4;
-  int y = 4;
+  int x = 16;
+  int y = 8;
   int z = 4;
-  //int x = 16;
-  //int y = 8;
-  //int z = 4;
   const int size = x * y * z;
   std::vector<float> test_data;
   test_data.resize(size);
@@ -39,7 +36,73 @@ TEST(sanity_check_float32, test_sanity_check_float32)
                        z);
 
   int rate = 8;
-  stream_set_rate(&zfp, rate, field->type, 3);
+  int dims = 3;
+  stream_set_rate(&zfp, rate, field->type, dims);
+  
+  size_t buffsize = zfp_stream_maximum_size(&zfp, field);
+
+  Word * d_stream;
+  cudaMalloc(&d_stream, buffsize);
+
+  unsigned char* buffer = new unsigned char[buffsize];
+  zfp.stream = d_stream;
+
+  compress(&zfp, field);
+
+  std::vector<float> test_data_out;
+  test_data_out.resize(size);
+
+
+  zfp_field *out_field;  
+
+  out_field = zfp_field_3d(&test_data_out[0], 
+                           zfp_type_float,
+                           x,
+                           y,
+                           z);
+
+  decompress(&zfp, out_field);
+
+  for(int i = 0; i < size; ++i)
+  {
+    ASSERT_TRUE(i == static_cast<int>(test_data_out[i]));
+  }
+
+  zfp_field_free(out_field);
+  zfp_field_free(field);
+  delete[] buffer;
+}
+
+TEST(t_mem_detection, t_host_mem_check)
+{
+  //
+  // this test is a simple sanity check to see if
+  // we can actually encode and decode with block size.
+  // that is not a multiple of four.
+  //
+  int x = 16;
+  int y = 8;
+  int z = 4;
+  const int size = x * y * z;
+  std::vector<float> test_data;
+  test_data.resize(size);
+  for(int i = 0; i < size; ++i)
+  {
+    test_data[i] = i; 
+  }
+
+  zfp_stream zfp;  
+  zfp_field *field;
+
+  field = zfp_field_3d(&test_data[0], 
+                       zfp_type_float,
+                       x,
+                       y,
+                       z);
+
+  int rate = 8;
+  int dims = 3;
+  stream_set_rate(&zfp, rate, field->type, dims);
 
   size_t buffsize = zfp_stream_maximum_size(&zfp, field);
   unsigned char* buffer = new unsigned char[buffsize];
@@ -61,7 +124,6 @@ TEST(sanity_check_float32, test_sanity_check_float32)
 
   for(int i = 0; i < size; ++i)
   {
-    std::cout<<i<<test_data_out[i]<<"\n";
     ASSERT_TRUE(i == static_cast<int>(test_data_out[i]));
   }
 
